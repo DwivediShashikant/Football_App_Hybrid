f52ca535c4dc93e967d73218bdfec38490c42bd3
DwivediShashikant <dwivedi.sk@outlook.com>
2018-01-25 23:41:43 +0530

    add side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component,ViewChild } from '@angular/core';
+import { Platform,Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { MyTeamsPage } from '../pages/pages';
@@ -10,13 +10,16 @@ import { MyTeamsPage } from '../pages/pages';
 })
 export class MyApp {
   rootPage:any = MyTeamsPage;
+  @ViewChild(Nav) nav: Nav;
+  
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+    this.initializeApp();
+  }
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+  initializeApp(){
+    this.platform.ready().then( ()=> {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
     });
   }
 }
